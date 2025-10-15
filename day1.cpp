af953357f003771255335fe992f26be0cd22d785
dexterrrrrrrrrrrrrrrrrrrrr <anuragpaul2006@gmail.com>
2025-10-15 20:22:59 +0530

    Initial commit

--- day1.cpp
+++ day1.cpp
@@ -1,3 +1,12 @@
+// Pattern Printing
+
+/* *****
+   *****
+   *****
+   *****
+   ***** */
+
+
 #include<iostream>
 using namespace std;
  void print1( int n){
@@ -8,6 +17,14 @@ using namespace std;
     }
  }
 
+/* 0000000
+   1111111
+   2222222
+   3333333
+   4444444
+   5555555
+   6666666 */
+
 
  void print2(int n ){
     for (int i = 0;i<n;i++){
@@ -16,10 +33,35 @@ using namespace std;
         }cout<<endl;
     }
  }
+
+/*      *
+       ***
+      *****
+     *******
+    *********  */
+
+ void print3(int n ){
+    for (int i = 0;i<n;i++){
+        for (int j = 0 ;j<n-i-1;j++){
+            cout<<" ";
+        }
+        for (int j = 0 ; j<2*i+1 ; j++){
+            cout<<"*";
+        }
+        for (int j = 0 ;j<n-i-1;j++){
+            cout<<" ";
+            
+        }cout<<endl;
+    }
+ }
+
  int main(){
     int n;
     cin>>n;
     print1(n);
+    cout<<"\n";
     print2(n);
+    cout<<"\n";
+    print3(n);
     return 0;   
  }
