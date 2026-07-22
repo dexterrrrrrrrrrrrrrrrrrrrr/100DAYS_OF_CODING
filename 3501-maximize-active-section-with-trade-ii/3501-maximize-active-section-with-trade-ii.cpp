2db522ea0a71c0b1f6cfc5b55e9c0d5b8384719b
ANURAG <anuragpaul2006@gmail.com>
2026-07-22 12:23:55 +0530

    Time: 66 ms (80.65%), Space: 259.1 MB (74.19%) - LeetHub

--- 3501-maximize-active-section-with-trade-ii/3501-maximize-active-section-with-trade-ii.cpp
+++ 3501-maximize-active-section-with-trade-ii/3501-maximize-active-section-with-trade-ii.cpp
@@ -1,115 +1,39 @@
-#include <ranges>
-
-struct Group {
-  int start;
-  int length;
-};
-
-class SparseTable {
- public:
-  SparseTable(const vector<int>& nums)
-      : n(nums.size()), st(std::bit_width(n) + 1, vector<int>(n + 1)) {
-    copy(nums.begin(), nums.end(), st[0].begin());
-    for (int i = 1; i <= bit_width(n); ++i)
-      for (int j = 0; j + (1 << i) <= n; ++j)
-        st[i][j] = max(st[i - 1][j], st[i - 1][j + (1 << (i - 1))]);
-  }
-
-  // Returns max(nums[l..r]).
-  int query(unsigned l, unsigned r) const {
-    const int i = bit_width(r - l + 1) - 1;
-    return max(st[i][l], st[i][r - (1 << i) + 1]);
-  }
-
- private:
-  const unsigned n;
-  vector<vector<int>> st;  // st[i][j] := max(nums[j..j + 2^i - 1])
-};
-
 class Solution {
- public:
-  vector<int> maxActiveSectionsAfterTrade(string s,
-                                          vector<vector<int>>& queries) {
-    const int n = s.length();
-    const int ones = ranges::count(s, '1');
-    const auto [zeroGroups, zeroGroupIndex] = getZeroGroups(s);
-    if (zeroGroups.empty())
-      return vector<int>(queries.size(), ones);
-
-    const SparseTable st(getZeroMergeLengths(zeroGroups));
-    vector<int> ans;
-
-    for (const vector<int>& query : queries) {
-      const int l = query[0];
-      const int r = query[1];
-      const int left = zeroGroupIndex[l] == -1
-                           ? -1
-                           : (zeroGroups[zeroGroupIndex[l]].length -
-                              (l - zeroGroups[zeroGroupIndex[l]].start));
-      const int right = zeroGroupIndex[r] == -1
-                            ? -1
-                            : (r - zeroGroups[zeroGroupIndex[r]].start + 1);
-      const auto [startAdjacentGroupIndex, endAdjacentGroupIndex] =
-          mapToAdjacentGroupIndices(
-              zeroGroupIndex[l] + 1,
-              s[r] == '1' ? zeroGroupIndex[r] : zeroGroupIndex[r] - 1);
-      int activeSections = ones;
-      if (s[l] == '0' && s[r] == '0' &&
-          zeroGroupIndex[l] + 1 == zeroGroupIndex[r])
-        activeSections = max(activeSections, ones + left + right);
-      else if (startAdjacentGroupIndex <= endAdjacentGroupIndex)
-        activeSections = max(
-            activeSections,
-            ones + st.query(startAdjacentGroupIndex, endAdjacentGroupIndex));
-      if (s[l] == '0' &&
-          zeroGroupIndex[l] + 1 <=
-              (s[r] == '1' ? zeroGroupIndex[r] : zeroGroupIndex[r] - 1))
-        activeSections =
-            max(activeSections,
-                ones + left + zeroGroups[zeroGroupIndex[l] + 1].length);
-      if (s[r] == '0' && zeroGroupIndex[l] < zeroGroupIndex[r] - 1)
-        activeSections =
-            max(activeSections,
-                ones + right + zeroGroups[zeroGroupIndex[r] - 1].length);
-      ans.push_back(activeSections);
+public:
+    vector<int> maxActiveSectionsAfterTrade(string str, vector<vector<int>>& queries) {
+        int n = str.size();
+        int a[n+10];    memset(a, 0, sizeof(a));
+        int s[n+10];    memset(s, 0, sizeof(s));
+        for (int i = 1; i <= n; i++) a[i] = (str[i-1]-'0'), s[i] = s[i-1] + a[i];
+        int K = 0, m = 0;
+        int idx[n+10], pos[n+10];  
+        int buf[n+10], siz[n+10];   memset(buf, 0, sizeof(buf)); memset(siz, 0xcf, sizeof(siz));
+        int kth[n+10], rnk[n+10];   memset(kth, 0, sizeof(kth)); memset(rnk, 0x3f, sizeof(rnk));
+        for (int l = 1, r = 1; l <= n; l = r) {    ++K, buf[K] = a[l], siz[K] = 0;
+            for (r = l; r <= n && a[r]==a[l]; r++) idx[r] = K, pos[r] = ++siz[K];
+            if (buf[K] == 1) ++m, kth[m] = K, rnk[K] = m;
+        }
+        int T = m ? log2(m) : 0;
+        int f[m+10][T+10];  
+        memset(f, 0, sizeof(f));
+        for (int i = 1; i <= m; i++) f[i][0] = siz[kth[i]-1] + siz[kth[i]+1];
+        for (int t = 1; t <= T; t++) {
+            for (int i = 1; i+(1<<t)-1 <= m; i++) f[i][t] = max(f[i][t-1], f[i+(1<<t-1)][t-1]);
+        }
+        auto ask = [&](int l, int r) -> int { int k = log2(r-l+1); return max(f[l][k], f[r-(1<<k)+1][k]); };
+        vector<int> ans;
+        for (auto& e : queries) {
+            int l = e[0]+1, r = e[1]+1;
+            int i = rnk[idx[l] + (a[l]^1)];
+            int j = rnk[idx[r] - (a[r]^1)];
+            if (j > m) j = 0;
+            int val = s[n];
+            if (i+1 < j)                            val = max(val, s[n] + ask(i+1, j-1));
+            if (a[l] == 0 && i < j)                 val = max(val, s[n] + (siz[kth[i]-1]-pos[l]+1) + siz[kth[i]+1]);
+            if (a[r] == 0 && i < j)                 val = max(val, s[n] + siz[kth[j]-1] + pos[r]);
+            if (a[l] == 0 && a[r] == 0 && i == j)   val = max(val, (r-l+1) + (s[l-1] + s[n]-s[r])); 
+            ans.push_back(val);
+        }
+        return ans;
     }
-
-    return ans;
-  }
-
- private:
-  // Returns the zero groups and the index of the zero group that contains the
-  // i-th character.
-  pair<vector<Group>, vector<int>> getZeroGroups(const string& s) {
-    vector<Group> zeroGroups;
-    vector<int> zeroGroupIndex;
-    for (int i = 0; i < s.length(); i++) {
-      if (s[i] == '0') {
-        if (i > 0 && s[i - 1] == '0')
-          ++zeroGroups.back().length;
-        else
-          zeroGroups.push_back({i, 1});
-      }
-      zeroGroupIndex.push_back(zeroGroups.size() - 1);
-    }
-    return {zeroGroups, zeroGroupIndex};
-  }
-
-  // Returns the sums of the lengths of the adjacent groups.
-  vector<int> getZeroMergeLengths(const vector<Group>& zeroGroups) {
-    vector<int> zeroMergeLengths;
-    for (const auto& [a, b] : zeroGroups | views::pairwise)
-      zeroMergeLengths.push_back(a.length + b.length);
-    return zeroMergeLengths;
-  }
-
-  // Returns the indices of the adjacent groups that contain l and r completely.
-  //
-  // e.g.    groupIndices = [0, 1, 2, 3]
-  // adjacentGroupIndices = [0 (0, 1), 1 (1, 2), 2 (2, 3)]
-  // map(startGroupIndex = 1, endGroupIndex = 3) -> (1, 2)
-  pair<int, int> mapToAdjacentGroupIndices(int startGroupIndex,
-                                           int endGroupIndex) {
-    return {startGroupIndex, endGroupIndex - 1};
-  }
 };
