ded4af7a7fa081b069f381f05d4c93e32e35e934
ANURAG <anuragpaul2006@gmail.com>
2026-07-28 13:40:33 +0530

    Time: 18 ms (91.43%), Space: 80.9 MB (9.89%) - LeetHub

---
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+        vector<int> freq(26,0);
+
+        for(char c : s){
+            freq[c-'a']++;
+        }
+
+        string left ="" , mid ="";
+
+        for(int i =0 ;i<26;i++){
+            left += string(freq[i]/2, char(i+'a'));
+
+            if(freq[i]%2)
+                mid=char(i+'a');
+        }
+
+        string right = left;
+
+        reverse(right.begin(),right.end());
+
+        return left + mid + right;
+    }
+};
